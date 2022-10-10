3fa0851a192dcc9fb82bbd86cb25d9d71cfa4f68
Amisha Srivastava <102937923+gem-amisha-srivastava@users.noreply.github.com>
2022-10-10 15:34:58 +0530

    Update and rename TestRunner.java to TestSuiteRunner.java
    Runner Updated

--- src/test/java/com/gemini/mis/runner/TestSuiteRunner.java
+++ src/test/java/com/gemini/mis/runner/TestSuiteRunner.java
@@ -5,10 +5,13 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
+
 @CucumberOptions(
-        features = "src/test/resources/features"
-        ,glue = "stepDefinitions",
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
 
 )
+
 public class TestRunner {
+        
 }
