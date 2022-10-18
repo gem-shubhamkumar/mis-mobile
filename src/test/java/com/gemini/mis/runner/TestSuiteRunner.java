f7bc8310bb78c828ed804ca4aa28efce2a2dcd00
Amisha Srivastava <102937923+gem-amisha-srivastava@users.noreply.github.com>
2022-10-18 10:30:29 +0530

    Update TestSuiteRunner.java

--- src/test/java/com/gemini/mis/runner/TestSuiteRunner.java
+++ src/test/java/com/gemini/mis/runner/TestSuiteRunner.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberSerenityRunner.class)
 @CucumberOptions(
-        features = "src/test/resources/features/" , glue = "com/gemini/mis/stepdefinitions",
-        tags = "@Dashboard"
+        features = "src/test/resources/features/OrgStruct.feature" , glue = "com/gemini/mis/stepdefinitions",
+        tags = "@TC01"
 )
 public class TestSuiteRunner
 {
