faaeb337e647b4c8f8e6bd3ff9147be2b5d539d9
gem-akhil-singh <akhil.rajput@geminisolutions.com>
2023-01-10 14:52:12 +0530

    Remove : Removed tag in testSuiteRunner

--- src/test/java/com/gemini/mis/runner/TestSuiteRunner.java
+++ src/test/java/com/gemini/mis/runner/TestSuiteRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberSerenityRunner.class)
 @CucumberOptions(
-        features = "src/test/resources/features/" , glue = "com/gemini/mis/stepdefinitions" , tags = "@lunch"
+        features = "src/test/resources/features/" , glue = "com/gemini/mis/stepdefinitions"
 
 
 )
